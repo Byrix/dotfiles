0adb1647fc113565d3f80781eac8f14e543cc72e
Byrix <Byrix99@pm.me>
2025-08-11 11:20:21 +1000

    basic functionality

---
+++ oh-my-zsh/dot-oh-my-zsh/custom/plugins/psql-import/psql-import.plugin.zsh
@@ -0,0 +1,47 @@
+set -a 
+source ${0:a:h}/.env 
+set +a
+
+function psqlimport() {
+  # $1 dbname
+  # $2 schema
+  # $3 folder
+  if [[ -z $1 ]]; then
+    echo "Please specify a database name"
+    return
+  else
+    local db=$1
+  fi
+
+  local file
+  local schema
+  if [[ -z $2 ]]; then
+    echo "Usage: psqlimport [dbname] (schema) [import folder]"
+    return
+  fi
+  if [[ -z $3 ]]; then
+    file=$2
+  else
+    schema=$2
+    file=$3
+  fi
+
+  local path=${0:a}/$file
+  if [[ -d $path ]]; then
+    echo "$path does not exist"
+    return
+  fi
+
+  local conn="host=localhost dbname=$db user=$USER password=$PASSWORD"
+  local ogr=$OGR_PATH/ogr2ogr
+  for f in ./$file/**/*.shp; do
+    echo "\n$f"
+    if [[ -z $schema ]]; then
+      $ogr -f "PostgreSQL" PG:"$conn" $f
+      [[ $? -ne 0 ]] && echo "Error $?"
+    else
+      $ogr -f "PostgreSQL" PG:"$conn" -lco SCHEMA=$schema $f
+      [[ $? -ne 0 ]] && echo "Error $?"
+    fi
+  done
+}
