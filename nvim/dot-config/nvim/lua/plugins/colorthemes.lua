496bc8182b047d16259957d183232300670b9295
Byrix <Byrix99@pm.me>
2025-09-16 18:56:46 +1000

    nvim switch to mocha theme

--- nvim/dot-config/nvim/lua/plugins/colorthemes.lua
+++ nvim/dot-config/nvim/lua/plugins/colorthemes.lua
@@ -10,8 +10,8 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- set colorscheme and overwrite highlights
-      vim.cmd.colorscheme 'catppuccin-macchiato'
-      local colors = require 'catppuccin.palettes.macchiato'
+      vim.cmd.colorscheme 'catppuccin-mocha'
+      local colors = require 'catppuccin.palettes.mocha'
       vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
       vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
       vim.api.nvim_set_hl(0, 'SpellBad', { fg = colors.base, bg = colors.maroon })
